3c1893421a45300fd737b6af76534f344098c90e
Petros Belachew <petrosbelachew@gmail.com>
2025-07-13 23:40:53 +0300

    Functions

--- Ifelse.py
+++ Ifelse.py
@@ -1,7 +1,19 @@
-temp=18
+temp=24
 
 if temp<=0:
     print('It Freezes')
+elif temp<18:
+    print('It is cold')
 
+elif temp < 25:
+    print('It is hot')
+
+elif temp >40:
+    print('too hot')
 else:
     print('It is not freezing')
+
+if True:
+    print(" dO SOMETHING")
+
+
