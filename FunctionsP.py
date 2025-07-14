dc9b3b1ecda8e2bd49caaf897be8ba8b987bad78
Petros Belachew <petrosbelachew@gmail.com>
2025-07-14 22:26:19 +0300

    While looping exercises

--- FunctionsP.py
+++ FunctionsP.py
@@ -3,28 +3,27 @@ Fill out the functions to calculate the area and circumference of a circle.
 Print the result to the user.
 """
 
-PI = 3.14159
-
+from math import pi
 
 def area(r):
-   return(PI * r **2)
+   return pi * r ** 2
 
 
 def circumference(r):
-     2 * PI * r
+     return 2 * PI * r
 
 
 def surfacearea():
-     4 * PI*r*r
+    return   4 * PI*r*r
 
 
 def Volume():
-   (4 / 3) * (PI * r * r * r)
+    return (4 / 3) * (PI * r * r * r)
 
 
-radius = input("Circle radius: ")
+radius = float(input("Circle radius: "))
 
 print(f'Area: {area(radius)}')  # <-- Call the area function and print the result
-print(f'Circumference: {circumference(r)}')  # <-- Call the circumference function and print
-print(f'SurfaceArea: {surfacearea(r)}')  # <-- Call the Surfacearea function and print the result
-print(f'Volume: {Volume(r)}')  # <-- Call the Volume function and print
+print(f'Circumference: {circumference(radius)}')  # <-- Call the circumference function and print
+print(f'SurfaceArea: {surfacearea(radius)}')  # <-- Call the Surfacearea function and print the result
+print(f'Volume: {Volume(radius}')  # <-- Call the Volume function and print
