da9a2b4160f422f076437af7e6749144969a7e0d
Petros Belachew <petrosbelachew@gmail.com>
2025-07-15 23:04:21 +0300

    looping exercises

---
+++ While_Problem.py
@@ -0,0 +1,14 @@
+
+
+def countdown(seconds):
+    # if time is five seconds, print happy new year
+
+
+    while seconds>0:
+        print(seconds)
+        seconds -= 1
+
+
+print("HNY")
+
+countdown(5)
---
+++ example_forloop.py
@@ -0,0 +1,6 @@
+primes=[2,3,7,11,13]
+prime_squares=[]
+
+for prime in primes:
+    prime_squares.append(prime ** 2)
+print(prime_squares)
