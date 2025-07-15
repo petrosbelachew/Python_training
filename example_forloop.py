17679114029347ede42c4941889e8d5f1be2bdc1
Petros Belachew <petrosbelachew@gmail.com>
2025-07-15 23:18:38 +0300

    Index added

--- example_forloop.py
+++ example_forloop.py
@@ -1,6 +1,5 @@
 primes=[2,3,7,11,13]
-prime_squares=[]
 
-for prime in primes:
-    prime_squares.append(prime ** 2)
-print(prime_squares)
+
+for i,prime in enumerate(primes):
+    print(f' {i}, {prime} squared is {prime **2}')
