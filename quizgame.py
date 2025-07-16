c27885a3d3be414e3a7106e63f703233eda873a5
Petros Belachew <petrosbelachew@gmail.com>
2025-07-16 21:33:40 +0300

    Initial commit with foundational structure of the game

---
+++ quizgame.py
@@ -0,0 +1,23 @@
+answers = [
+    'vanuatu',
+    'vatican City',
+    'venezuela',
+    'vietnam'
+]
+
+guessed = []
+
+tes=[]
+i=0
+while tes != 'q':
+    guess = input(f'Enter answer for trial and q to end: ')
+    guessed.append(guess)
+
+for guess in guessed:
+
+    i += 1
+    print(f'ypu are left with {5 - i} trials')
+    if i == 5:
+         break
+
+print(f'Test')
