443a97fe2128bcefd1511c33f1fb8c294647c6d4
Petros Belachew <petrosbelachew@gmail.com>
2025-07-18 16:03:33 +0300

    Initial commit with foundational structure of the game

---
+++ pydic.py
@@ -0,0 +1,3 @@
+my_dic = {'A': 1, 'B': 2}
+for key, value in my_dic.items():
+    print(f"{key}: {value}")
--- quizgame.py
+++ quizgame.py
@@ -7,17 +7,19 @@ answers = [
 
 guessed = []
 
-tes=[]
-i=0
+tes = []
+i = 0
 while tes != 'q':
     guess = input(f'Enter answer for trial and q to end: ')
     guessed.append(guess)
+
+ if tes==q:
 
 for guess in guessed:
 
     i += 1
     print(f'ypu are left with {5 - i} trials')
     if i == 5:
-         break
+        break
 
 print(f'Test')
