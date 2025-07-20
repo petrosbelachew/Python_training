e14201701c0a043437c323b2d486402c02515b15
Petros Belachew <petrosbelachew@gmail.com>
2025-07-20 20:20:21 +0300

    Classes, Object, and Methods

--- classes.py
+++ classes.py
@@ -1,10 +1,11 @@
 class MyClass:
-    ...
+   def a_method(self):
+       print(f'called a_method on {self} ')
 
 
 class Horse:
-    ...
-
+   def trot(self):
+       print('clop clop clop')
 
 obj=MyClass()
 horse1=Horse()
@@ -14,4 +15,6 @@ print(obj)
 print(horse1)
 print(str1)
 print(type(horse1))
+obj.a_method()
+horse1.trot()
 
